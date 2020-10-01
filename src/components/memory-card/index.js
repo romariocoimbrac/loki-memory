4e306f1d5bf265a40bb263ab55bcd0e08262cef3
Romario Coimbra <romariocoimbrac@gmail.com>
2020-09-30 23:30:25 -0300

    Started Memory-game

---
+++ src/components/memory-card/index.js
@@ -0,0 +1,64 @@
+function MemoryCard() {
+  const $head = document.querySelector("head");
+  const $style = document.createElement("style");
+  $style.textContent = `
+    div {
+      width: 155px;
+      height: 155px;
+      position: relative;
+    }
+    .memory-card .card {
+      width: 100%;
+      height: 100%;
+      background-color: #b4b4b4;
+      border-radius: 30px;
+      box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16);
+      cursor: pointer;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: absolute;
+    }
+    .memory-card.-active .card {
+      display: none;
+    }
+    .memory-card.-active .card.-front{
+      display: flex;
+    }
+    .memory-card .card.-front {
+      background-color: #fff;
+    }
+    .memory-card .card.-front::before {
+      content: "";
+      width: 95px;
+      height: 95px;
+      background-color: #d4d4d4;
+      border-radius: 50%;
+      position: absolute;
+    }
+    .memory-card .card > .icon {
+      width: 100px;
+    }
+    .memory-card .card.-front > .icon {
+      position: absolute;
+      transform: translateY(-10px);
+    }
+  `;
+  $head.insertBefore($style, null);
+
+  return ({ src, alt }) => `
+  <div class="memory-card" onClick="handleClick(this)">
+    <article class="card -front">
+      <img src=${src} alt=${alt}
+      class="icon" 
+      />
+    </article>
+    <article class="card">
+      <img src="img/loki-card.png" alt="Loki Card"
+      class="icon"
+      />
+  </article>
+</div>`;
+}
+
+const handleClick = ($component) => $component.classList.toggle("-active");
---
+++ src/pages/game/index.js
@@ -0,0 +1,33 @@
+const $root = document.querySelector("#root");
+
+const $cardsWrapper = createCardsWrapper();
+
+const createMemoryCard = MemoryCard();
+
+const $memoryCardHype = createMemoryCard({
+  src: "img/loki-hype.png",
+  alt: "Loki Hype",
+});
+const $memoryCardNormal = createMemoryCard({
+  src: "img/loki-normal.png",
+  alt: "Loki Normal",
+});
+const $memoryCardSmile = createMemoryCard({
+  src: "img/loki-smile.png",
+  alt: "Loki Smile",
+});
+const $memoryCardTall = createMemoryCard({
+  src: "img/loki-tall.png",
+  alt: "Loki Tall",
+});
+
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardHype);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardHype);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardNormal);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardNormal);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardSmile);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardSmile);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardTall);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardTall);
+
+$root.insertAdjacentElement("beforeend", $cardsWrapper);
